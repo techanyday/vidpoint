fb043e732535e96d6be470ed883af18c0cc32f55
techanyday <youremail@example.com>
2025-01-21 17:22:26 +0000

    Fix app export for Gunicorn

--- app.py
+++ app.py
@@ -247,6 +247,8 @@ def create_app():
 
     return app
 
+# Create the application instance for Gunicorn
+app = create_app()
+
 if __name__ == '__main__':
-    app = create_app()
-    app.run(host='0.0.0.0', debug=True, port=5000)
+    app.run(debug=True)
